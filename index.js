7b9c19f964ab750e14c49629ff6dfcddabc01ea7
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-08-17 19:39:24 +0200

    dynamic choice

--- index.js
+++ index.js
@@ -5,32 +5,68 @@ const cheerio = require("cheerio");
 
 const app = express();
 
+const pages = [
+  {
+    name: "bbc",
+    address: "https://www.bbc.com/sport/football",
+  },
+];
+
 const articles = [];
 
+pages.forEach((page) => {
+  axios.get(page.address).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    $('a:contains("Arsenal")', html).each(function () {
+      const title = $(this).text();
+      const url = $(this).attr("href");
+
+      articles.push({
+        title,
+        url,
+        source: page.name,
+      });
+    });
+  });
+});
+
 app.get("/", (req, res) => {
-  res.json("Welcome to my BBC News API");
+  res.json("Welcome to my Premier League News API");
 });
 
 app.get("/football", (req, res) => {
+  res.json(articles);
+});
+
+app.get("/football/:clubId", async (req, res) => {
+  const clubId = req.params.clubId;
+
+  const formattedClubId =
+    clubId.charAt(0).toLocaleUpperCase + clubId.slice(1).toLocaleLowerCase();
+
   axios
-    .get("https://www.bbc.com/sport/football")
+    .get(formattedClubId)
     .then((response) => {
       const html = response.data;
       const $ = cheerio.load(html);
+      const clubNews = [];
 
-      $('a:contains("Arsenal")', html).each(function () {
+      const clubQuery = 'a:contains("Chelsea")';
+
+      $(clubQuery, html).each(function () {
         const title = $(this).text();
         const url = $(this).attr("href");
-        articles.push({
+        clubNews.push({
           title,
           url,
+          // source: clubId
         });
       });
-      res.json(articles);
+      res.json(clubNews);
     })
-    .catch((err) => {
-      console.log(err);
-    });
+    .catch((err) => console.log(err));
 });
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
