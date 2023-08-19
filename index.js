4f7836c884565692c492944d2b9d81dbe6493d74
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-08-19 21:23:44 +0200

    getting data from multiple sources

--- index.js
+++ index.js
@@ -5,42 +5,35 @@ const cheerio = require("cheerio");
 
 const app = express();
 
+let clubNews = [];
 const pages = [
   {
     name: "bbc",
-    address: "https://www.bbc.com/sport/football",
+    address: "https://www.bbc.com/sport/football/premier-league",
+  },
+  {
+    name: "skysports",
+    address: "https://www.skysports.com/premier-league-news",
+  },
+  {
+    name: "dailymail",
+    address: "https://www.dailymail.co.uk/sport/premierleague/index.html",
+  },
+  {
+    name: "theguardian",
+    address: "https://www.theguardian.com/football/premierleague",
+  },
+  {
+    name: "espn",
+    address: "https://www.espn.com/soccer/league/_/name/eng.1",
   },
 ];
-
-const articles = [];
-
-pages.forEach((page) => {
-  axios.get(page.address).then((response) => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-
-    $('a:contains("Arsenal")', html).each(function () {
-      const title = $(this).text();
-      const url = $(this).attr("href");
-
-      articles.push({
-        title,
-        url,
-        source: page.name,
-      });
-    });
-  });
-});
 
 app.get("/", (req, res) => {
   res.json("Welcome to my Premier League News API");
 });
 
-app.get("/football", (req, res) => {
-  res.json(articles);
-});
-
-app.get("/football/:clubId", async (req, res) => {
+app.get("/:clubId", (req, res) => {
   const clubId = req.params.clubId;
 
   const formattedClubId = clubId
@@ -48,27 +41,37 @@ app.get("/football/:clubId", async (req, res) => {
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
     .join(" ");
 
-  axios
-    .get("https://www.bbc.com/sport/football")
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      const clubNews = [];
+  pages.forEach((page) => {
+    axios
+      .get(page.address)
+      .then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
 
-      const clubQuery = `a:contains(${formattedClubId})`;
+        const clubQuery = `a:contains(${formattedClubId})`;
 
-      $(clubQuery, html).each(function () {
-        const title = $(this).text();
-        const url = $(this).attr("href");
-        clubNews.push({
-          title,
-          url,
-          // source: clubId
+        $(clubQuery, html).each(function () {
+          const title = $(this)
+            .text()
+            .replace(/[\r\n]+/gm, "") // remove line breaks
+            .trim()
+            .split(" ")
+            .map((word) => word.trim())
+            .join(" ");
+          const url = $(this).attr("href");
+
+          if (title === formattedClubId) return; // return when te link direct to general club info
+
+          clubNews.push({
+            title,
+            url,
+            source: page.name,
+          });
         });
-      });
-      res.json(clubNews);
-    })
-    .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+  });
+  res.json(clubNews);
 });
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
